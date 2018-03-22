4d69e29746e51456deca6a90444bb3e10fd3d25e
Shogo Matsumoto <sougyo2001@gmail.com>
2018-03-22 23:46:45 +0900

    allow x2csv to handle stdin and plot.py to plot multiple graphs

--- plot.py
+++ plot.py
@@ -7,34 +7,62 @@ import matplotlib.ticker as ticker
 import argparse
 
 parser = argparse.ArgumentParser(description='performance info converter')
-parser.add_argument('-w'          , default=None, type=str, help='output file')
-parser.add_argument('--regex'     , default=None, type=str, help='column filter regexp')
-parser.add_argument('--ylim_min'  , default=None, type=int, help='ylim min')
-parser.add_argument('--ylim_max'  , default=None, type=int, help='ylim max')
-parser.add_argument('--ylabel'    , default=None, type=str, help='ylabel')
-parser.add_argument('--title'     , default=None, type=str, help='graph title')
-parser.add_argument('--stack'     , action='store_true', help='stacked graph')
-parser.add_argument('--width'     , default=11,   type=int, help='width')
-parser.add_argument('--height'    , default=6,    type=int, help='height')
-parser.add_argument('--yformatter', default="{x:,.0f}", type=str, help='y axis formatter')
-parser.add_argument('--rot'       , default=0, type=str, help='rot')
+parser.add_argument('-w'           , default=None, type=str, help='output image file')
+parser.add_argument('--regex'      , default=None, type=str, help='column filter regexp')
+parser.add_argument('--ylim_min'   , default=None, type=int, help='ylim min')
+parser.add_argument('--ylim_max'   , default=None, type=int, help='ylim max')
+parser.add_argument('--ylabel'     , default=None, type=str, help='ylabel')
+parser.add_argument('--title'      , default=None, type=str, help='graph title')
+parser.add_argument('--stack'      , action='store_true', help='stacked graph')
+parser.add_argument('--width'      , default=11,   type=int, help='width')
+parser.add_argument('--height'     , default=6,    type=int, help='height')
+parser.add_argument('--yformatter' , default="{x:,.0f}", type=str, help='y axis formatter')
+parser.add_argument('--rot'        , default=0, type=str, help='rot')
+parser.add_argument('--legend_loc' , default=None, type=str, help='legend location "(upper|lower|center) (left|right|center)"')
+parser.add_argument('--legend_uniq', action='store_true', help='show legend only on the bottom of graph')
+parser.add_argument('filepaths'    , nargs='*')
 args = parser.parse_args()
 
-df = pd.read_csv(sys.stdin, index_col='time')
-if args.regex:
-  df = df.filter(regex=args.regex)
+filepaths = args.filepaths or [sys.stdin]
 
-plot_option = {}
-plot_option["rot"] = args.rot
-plot_option["figsize"] = (args.width, args.height)
-if args.stack:
-  plot_option['kind'] = 'area'
-  plot_option['stacked'] = True
-  plot_option['linewidth'] = 0
+dfs = [pd.read_csv(f, index_col='time') for f in filepaths]
+dfs = [(df.filter(regex=args.regex) if args.regex else df) for df in dfs]
 
-ax = df.plot(**plot_option)
-ax.set_xticklabels(df.index)
-ax.yaxis.set_major_formatter(ticker.StrMethodFormatter(args.yformatter))
+fig, ax_list = plt.subplots(nrows=len(dfs))
+if len(dfs) == 1:
+  ax_list = [ax_list]
+
+ylims = []
+for df, ax in zip(dfs, ax_list):
+  plot_option = {}
+  plot_option["rot"] = args.rot
+  plot_option["figsize"] = (args.width, args.height)
+  plot_option["ax"] = ax
+  if args.stack:
+    plot_option['kind'] = 'area'
+    plot_option['stacked'] = True
+    plot_option['linewidth'] = 0
+  ax.set_xticklabels(df.index)
+  ax.yaxis.set_major_formatter(ticker.StrMethodFormatter(args.yformatter))
+  df.plot(**plot_option)
+
+  ylims.append(ax.get_ylim())
+
+y_min = min([y[0] for y in ylims])
+y_max = max([y[1] for y in ylims])
+if args.ylim_min != None:
+  y_min = args.ylim_min
+if args.ylim_max != None:
+  y_max = args.ylim_max
+
+for i, ax in enumerate(ax_list):
+  ax.set_ylim(y_min, y_max)
+
+  if args.legend_uniq and (i != 0):
+    ax.legend([""])
+  elif args.legend_loc:
+    ax.legend(loc=args.legend_loc)
+
 
 if args.ylabel:
   plt.ylabel(args.ylabel)
@@ -42,17 +70,8 @@ if args.ylabel:
 if args.title:
   plt.title(args.title)
 
-ymin, ymax = plt.ylim()
-if args.ylim_min != None:
-  ymin = args.ylim_min
-if args.ylim_max != None:
-  ymax = args.ylim_max
-plt.ylim(ymin, ymax)
-
 if args.w:
-#  ax.get_figure().savefig(args.w)
   plt.savefig(args.w)
 else:
   plt.show()
-
 
--- x2csv.py
+++ x2csv.py
@@ -15,24 +15,25 @@ names = ', '.join([k for k in table_yaml])
 
 parser = argparse.ArgumentParser(description='performance info converter')
 parser.add_argument('-t', default=None, help=("specify a type of perftool: " + names))
-parser.add_argument('filepath')
+parser.add_argument('filepath', nargs='?')
 args = parser.parse_args()
 
 key_list = []
 if args.t:
   key_list.extend(filter(lambda k: re.search(args.t, k), table_yaml))
 
-basename = os.path.basename(args.filepath)
+basename = os.path.basename(args.filepath) if args.filepath else 'sar'
 for l in map(lambda h: h["list"], filter(lambda x: re.search(x["pattern"], basename), hint_yaml)):
   key_list.extend(l)
 
 writer = csv.writer(sys.stdout)
 out = False
 for k in key_list:
-  with open(args.filepath) as f:
-    for a in lib.table.Table(f, **table_yaml[k]):
-      writer.writerow(a)
-      out = True
+  f = open(args.filepath) if args.filepath else sys.stdin
+  for a in lib.table.Table(f, **table_yaml[k]):
+    writer.writerow(a)
+    sys.stdout.flush()
+    out = True
   if out:
     break
 
