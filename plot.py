f96343cbd3637bc4633758dfdd32aa25b6549749
Shogo Matsumoto <sougyo2001@gmail.com>
2018-08-20 20:18:50 +0900

    add top_n and show_title option to plot.py

--- plot.py
+++ plot.py
@@ -7,20 +7,23 @@ import matplotlib.ticker as ticker
 import argparse
 
 parser = argparse.ArgumentParser(description='performance info converter')
-parser.add_argument('-w'           , default=None, type=str, help='output image file')
-parser.add_argument('--regex'      , default=None, type=str, help='column filter regexp')
-parser.add_argument('--ylim_min'   , default=None, type=int, help='ylim min')
-parser.add_argument('--ylim_max'   , default=None, type=int, help='ylim max')
-parser.add_argument('--ylabel'     , default=None, type=str, help='ylabel')
-parser.add_argument('--title'      , default=None, type=str, help='graph title')
-parser.add_argument('--stack'      , action='store_true', help='stacked graph')
-parser.add_argument('--width'      , default=11,   type=int, help='width')
-parser.add_argument('--height'     , default=6,    type=int, help='height')
-parser.add_argument('--yformatter' , default="{x:,.0f}", type=str, help='y axis formatter')
-parser.add_argument('--rot'        , default=0, type=str, help='rot')
-parser.add_argument('--legend_loc' , default=None, type=str, help='legend location "(upper|lower|center) (left|right|center)"')
-parser.add_argument('--legend_uniq', action='store_true', help='show legend only on the bottom of graph')
-parser.add_argument('filepaths'    , nargs='*')
+parser.add_argument('-w'           ,type=str, help='output image file')
+parser.add_argument('--regex'      ,type=str, help='column filter regexp')
+parser.add_argument('--ylim_min'   ,type=int, help='ylim min')
+parser.add_argument('--ylim_max'   ,type=int, help='ylim max')
+parser.add_argument('--avg_top_n'  ,type=int, help='show only top n result by average')
+parser.add_argument('--max_top_n'  ,type=int, help='show only top n result by max')
+parser.add_argument('--ylabel'     ,type=str, help='ylabel')
+parser.add_argument('--title'      ,type=str, help='graph title')
+parser.add_argument('--stack'      ,action='store_true', help='stacked graph')
+parser.add_argument('--show_title' ,action='store_true', help='show title in each graph')
+parser.add_argument('--width'      ,default=11,   type=int, help='width')
+parser.add_argument('--height'     ,default=6,    type=int, help='height')
+parser.add_argument('--yformatter' ,default="{x:,.0f}", type=str, help='y axis formatter')
+parser.add_argument('--rot'        ,default=0, type=str, help='rot')
+parser.add_argument('--legend_loc' ,default=None, type=str, help='legend location "(upper|lower|center) (left|right|center)"')
+parser.add_argument('--legend_uniq',action='store_true', help='show legend only on the bottom of graph')
+parser.add_argument('filepaths'    ,nargs='*')
 args = parser.parse_args()
 
 filepaths = args.filepaths or [sys.stdin]
@@ -28,12 +31,17 @@ filepaths = args.filepaths or [sys.stdin]
 dfs = [pd.read_csv(f, index_col='time') for f in filepaths]
 dfs = [(df.filter(regex=args.regex) if args.regex else df) for df in dfs]
 
+if args.avg_top_n:
+  dfs = [df[df.mean().sort_values(ascending=False)[:args.avg_top_n].index] for df in dfs]
+elif args.max_top_n:
+  dfs = [df[df.max().sort_values(ascending=False)[:args.max_top_n].index] for df in dfs]
+
 fig, ax_list = plt.subplots(nrows=len(dfs))
 if len(dfs) == 1:
   ax_list = [ax_list]
 
 ylims = []
-for df, ax in zip(dfs, ax_list):
+for i, (df, ax) in enumerate(zip(dfs, ax_list)):
   plot_option = {}
   plot_option["rot"] = args.rot
   plot_option["figsize"] = (args.width, args.height)
@@ -44,6 +52,8 @@ for df, ax in zip(dfs, ax_list):
     plot_option['linewidth'] = 0
   ax.set_xticklabels(df.index)
   ax.yaxis.set_major_formatter(ticker.StrMethodFormatter(args.yformatter))
+  if args.show_title:
+    ax.set_title(filepaths[i])
   df.plot(**plot_option)
 
   ylims.append(ax.get_ylim())
