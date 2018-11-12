49f353b05eeb9f7c0387e6105a98817fd7c888c9
Shogo Matsumoto <sougyo2001@gmail.com>
2018-11-12 18:59:21 +0900

    add default option on csv2matrix

--- csv2matrix.py
+++ csv2matrix.py
@@ -6,8 +6,7 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--prefix' , type=str, help='prefix_key')
-parser.add_argument('--postfix', type=str, help='postfix_key')
+parser.add_argument('--default', type=float, help='default value of N/A')
 args = parser.parse_args()
 
 d = {}
@@ -20,30 +19,29 @@ for (time, k1, k2, val) in csv.reader(sys.stdin):
   if time != prev_time:
     cnt += 1
 
-  internal_time = (cnt, time)
+  cnt_time = (cnt, time)
 
   key = str(k1)
   if k2:
    key += "." + str(k2)
 
-  if not (internal_time in d):
-    d[internal_time] = {}
+  if not (cnt_time in d):
+    d[cnt_time] = {}
 
-  d[internal_time][key] = val
+  d[cnt_time][key] = val
 
-  time_list[internal_time] = True
-  key_list[key]            = True
+  time_list[cnt_time] = True
+  key_list[key]       = True
 
   prev_time = time
 
-column_names = key_list.keys()
-if args.prefix:
-  column_names = [("%s%s" % (args.prefix, k))  for k in column_names]
-if args.postfix:
-  column_names = [("%s%s" % (k, args.postfix)) for k in column_names]
+def val_or_default(v):
+  if (v is None) and (args.default is not None):
+    return args.default
+  return v
 
 writer = csv.writer(sys.stdout)
-writer.writerow(["time"] + list(column_names))
-for internal_time in time_list:
-  (cnt, time) = internal_time
-  writer.writerow([time] + [d[internal_time].get(key) for key in key_list])
+writer.writerow(["time"] + list(key_list.keys()))
+for cnt_time in time_list:
+  (cnt, time) = cnt_time
+  writer.writerow([time] + [val_or_default(d[cnt_time].get(key)) for key in key_list])
